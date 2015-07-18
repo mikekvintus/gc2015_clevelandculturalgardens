884e6b3923d2e4b52f978698daf02c7387d24c23
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2015-07-18 15:56:28 -0400

    continue removing unused code

--- site/wp-content/themes/catch-evolution-child/js/main.js
+++ site/wp-content/themes/catch-evolution-child/js/main.js
@@ -11,16 +11,9 @@
 // todo: feature switch on geolocation. (browser check)
 // todo: handlers for watchPosition success/fail
 jQuery(document).ready(function() {
-	var mapOptions = { 
-			enableHighAccuracy: true,
-			timeout: Infinity,
-			maximumAge: 0
-	};
-	CleGardens.map = new google.maps.Map(document.getElementById("wpgmza_map_1"), mapOptions);
-    
-	  loadJS('https://google-maps-utility-library-v3.googlecode.com/svn/trunk/geolocationmarker/src/geolocationmarker-compiled.js', function() { 
-		CleGardens.geoMarker = new GeolocationMarker();
-		CleGardens.geoMarker.setMap(MYMAP[1].map);
+  loadJS('https://google-maps-utility-library-v3.googlecode.com/svn/trunk/geolocationmarker/src/geolocationmarker-compiled.js', function() { 
+	    CleGardens.geoMarker = new GeolocationMarker();
+      CleGardens.geoMarker.setMap(MYMAP[1].map);
 	});
 });
 
@@ -43,15 +36,5 @@ var CleGardens = function() {
 	var currentPosition = null;
 	var geoMarker = null;
 
-	var success = function(position) {
-		currentPosition = position;
-	};
-	
-	var fail = function() { error({code: 1, message: "Failed on geolocation.watchPosition()"});};
-
-	function error(err) {
-	 console.warn('ERROR(' + err.code + '): ' + err.message);
-	}
-	
-	return {success: success, error: error, map: map, position: currentPosition, geoMarker: geoMarker};
+	return {map: map, position: currentPosition, geoMarker: geoMarker};
 };
